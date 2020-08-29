147c73b85e970de575662812b84e99ed7c26f9a4
Alex <alexydenkov99@gmail.com>
2020-08-29 12:04:45 +0700

    syka rabotaet

--- app/src/main/java/com/example/projectdeveloperslife/MainActivity.kt
+++ app/src/main/java/com/example/projectdeveloperslife/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.projectdeveloperslife
 
+import android.app.Activity
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide;
+import java.security.AccessController.getContext
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var next: ImageView
     private lateinit var prev: ImageView
     private lateinit var Image: ImageView
+    private lateinit var Image1: ImageView
     //private val progressDialog: ImageView =
 
 
@@ -17,24 +23,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //
+
         setContentView(R.layout.activity_main)
+        Image = findViewById(R.id.imageView)
+        unit()
+        /*Glide.with(applicationContext)
+            .load("https://static.devli.ru/public/images/previews/201308/d0906011-2d12-489a-af69-d9446402c2ed.jpg")
+            .into(Image)
+*/
 
     }
     private fun unit(){
         next = findViewById(R.id.imageView4)
         prev = findViewById(R.id.imageView3)
-        Image =findViewById(R.id.imageView2)
-        val context = getApplicationContext();
-
+        Image =findViewById(R.id.okno)
+        val context = applicationContext
 
 
         //обработка нажатия на кнопку next
         next.setOnClickListener(View.OnClickListener {
-            presenter.comtex = applicationContext
+            presenter.comtex = context
             presenter.image = Image
-            presenter.ad() })
+            presenter.ad()
+            })
 
-        //обработка на жатия на кнопку prev
-        next.setOnClickListener(View.OnClickListener { presenter.ad() })
+            //обработка на жатия на кнопку prev
+            //next.setOnClickListener(View.OnClickListener { presenter.ad() }
+
     }
 }
